b5a60e6669fc24b411b85ba92e7116e919266243
Sokratis Vogiatzakis <sokvogiatzakis@gmail.com>
2021-02-17 22:07:05 +0200

    Imported Equipment/UI/Inventory/Drop/Pickup/Tooltips/Actions from the tutorial.

---
+++ Assets/GameDev.tv Assets/Scripts/Inventories/EquipLocation.cs
@@ -0,0 +1,17 @@
+namespace RPG.Inventories
+{
+    /// <summary>
+    /// Locations on the players body where items can be equipped.
+    /// </summary>
+    public enum EquipLocation
+    {
+        Helmet,
+        Necklace,
+        Body,
+        Trousers,
+        Boots,
+        Weapon,
+        Shield,
+        Gloves,
+    }
+}
---
+++ Assets/GameDev.tv Assets/Scripts/UI/Inventories/IItemHolder.cs
@@ -0,0 +1,12 @@
+﻿using RPG.Inventories;
+
+namespace RPG.UI.Inventories
+{
+    /// <summary>
+    /// Allows the `ItemTooltipSpawner` to display the right information.
+    /// </summary>
+    public interface IItemHolder
+    {
+        InventoryItem GetItem();
+    }
+}
