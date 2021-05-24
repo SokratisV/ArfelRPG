d32f5e643904e9f0fb4ceb4a87f5f7d66525d0f7
Sokratis Vogiatzakis <sokvogiatzakis@gmail.com>
2021-05-24 22:27:24 +0300

    Testing of multiple (idle) animation.

---
+++ Assets/Scripts/Combat/RandomAnimationBehavior.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace RPG.Combat
+{
+	public class RandomAnimationBehavior : StateMachineBehaviour
+	{
+		[SerializeField] private int amountOfAnimations;
+		[SerializeField] private string animationId;
+		[SerializeField] private bool onEnter;
+		
+		private int _animationId = -1;
+
+		public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+		{
+			if (!onEnter) return;
+			if (_animationId == -1) _animationId = Animator.StringToHash(animationId);
+			var value = Random.Range(0, amountOfAnimations);
+			animator.SetInteger(_animationId, value);
+		}
+
+		public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+		{
+			if (onEnter) return;
+			if (_animationId == -1) _animationId = Animator.StringToHash(animationId);
+			var value = Random.Range(0, amountOfAnimations);
+			animator.SetInteger(_animationId, value);
+		}
+	}
+}
