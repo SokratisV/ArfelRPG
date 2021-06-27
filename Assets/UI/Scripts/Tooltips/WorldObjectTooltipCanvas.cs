2048ef002d38d43cb26aa107314fbdd5d9c0e2b7
Sokratis Vogiatzakis <sokvogiatzakis@gmail.com>
2021-06-27 15:07:46 +0300

    Reduced file size of most audio files + terrains, small tooltip fix.

--- Assets/UI/Scripts/Tooltips/WorldObjectTooltipCanvas.cs
+++ Assets/UI/Scripts/Tooltips/WorldObjectTooltipCanvas.cs
@@ -4,6 +4,7 @@ using RPG.Core;
 using RPG.Core.SystemEvents;
 using RPG.UI.Tooltips;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace UI.Scripts.Tooltips
 {
@@ -12,19 +13,13 @@ namespace UI.Scripts.Tooltips
 	{
 		[Tooltip("The prefab of the tooltip to spawn.")] [SerializeField]
 		private GameObject tooltipPrefab = null;
-
 		[SerializeField] private Vector3 tooltipOffset;
 
 		private GameObject _tooltip = null;
 		private Canvas _canvas;
-		private RectTransform _canvasRect;
 		private Coroutine _followCursorRoutine;
 
-		private void Awake()
-		{
-			_canvas = GetComponent<Canvas>();
-			_canvasRect = _canvas.GetComponent<RectTransform>();
-		}
+		private void Awake() => _canvas = GetComponent<Canvas>();
 
 		public void OnWorldObjectNotification(WorldObjectTooltipData data)
 		{
@@ -45,7 +40,8 @@ namespace UI.Scripts.Tooltips
 			}
 		}
 
-		public void UpdateTooltip(GameObject tooltip, WorldObjectTooltipData data) => tooltip.GetComponent<WorldObjectTooltip>().Setup(data);
+		private void UpdateTooltip(GameObject tooltip, WorldObjectTooltipData data) => tooltip.GetComponent<WorldObjectTooltip>().Setup(data);
+		private void PositionTooltip() => _followCursorRoutine = _followCursorRoutine.StartCoroutine(this, FollowCursor());
 
 		private void ClearTooltip()
 		{
@@ -57,26 +53,14 @@ namespace UI.Scripts.Tooltips
 			}
 		}
 
-		private void PositionTooltip() => _followCursorRoutine = _followCursorRoutine.StartCoroutine(this, FollowCursor());
-
 		private IEnumerator FollowCursor()
 		{
 			while (_tooltip)
 			{
+				_tooltip.SetActive(!EventSystem.current.IsPointerOverGameObject());
 				_tooltip.transform.position = Input.mousePosition + tooltipOffset;
 				yield return null;
 			}
-		}
-
-		private int GetCornerIndex(bool below, bool right)
-		{
-			return below switch
-			{
-				true when !right => 0,
-				false when !right => 1,
-				false when true => 2,
-				_ => 3
-			};
 		}
 	}
 }
